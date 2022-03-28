85ad822446a7ff0082593300e0e084dfbc91e6cb
Răzvan Morcov-Pahoncea <razvan10190@gmail.com>
2022-03-28 17:40:51 +0300

    updated var names

--- Laboratory 6/src/main/java/compulsory/panels/DrawingPanel.java
+++ Laboratory 6/src/main/java/compulsory/panels/DrawingPanel.java
@@ -37,26 +37,26 @@ public class DrawingPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics graphics) {
-        Graphics2D g = (Graphics2D) graphics;
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, canvasWidth, canvasHeight);
-        paintGrid(g);
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(0, 0, canvasWidth, canvasHeight);
+        paintGrid(graphics2D);
     }
 
-    private void paintGrid(Graphics2D g) {
-        g.setColor(Color.DARK_GRAY);
+    private void paintGrid(Graphics2D graphics2D) {
+        graphics2D.setColor(Color.DARK_GRAY);
         for (int row = 0; row < rows; row++) {
             int x1 = paddingX;
             int y1 = paddingY + row * cellHeight;
             int x2 = paddingX + boardWidth;
-            g.drawLine(x1, y1, x2, y1);
+            graphics2D.drawLine(x1, y1, x2, y1);
         }
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 int x = paddingX + col * cellWidth;
                 int y = paddingY + row * cellHeight;
-                g.setColor(Color.LIGHT_GRAY);
-                g.drawOval(x - stoneSize / 2, y - stoneSize / 2, stoneSize, stoneSize);
+                graphics2D.setColor(Color.LIGHT_GRAY);
+                graphics2D.drawOval(x - stoneSize / 2, y - stoneSize / 2, stoneSize, stoneSize);
             }
         }
     }
