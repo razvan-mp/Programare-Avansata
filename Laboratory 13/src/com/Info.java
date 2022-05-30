5feba294aa3dbae5f82ccfdedb53ff4525ba4a00
Răzvan Morcov-Pahoncea <razvan10190@gmail.com>
2022-05-30 17:49:26 +0300

    modified Info class

--- Laboratory 13/src/com/Info.java
+++ Laboratory 13/src/com/Info.java
@@ -14,16 +14,15 @@ public class Info {
         System.out.println(message.getLanguage() + " " + locale.getLanguage());
         System.out.println(message.getCurrency() + " " + Currency.getInstance(locale));
         var dateFormatSymbols = DateFormatSymbols.getInstance(locale);
+
         System.out.print(message.getWeek() + " ");
-        for (int i = 1; i < 7; i++) {
-            System.out.print(dateFormatSymbols.getWeekdays()[i] + ", ");
-        }
-        System.out.println(dateFormatSymbols.getWeekdays()[7]);
+        for (var weekday : dateFormatSymbols.getWeekdays())
+            System.out.println(weekday + ", ");
+
         System.out.print(message.getMonths() + " ");
-        for (int i = 0; i < 12; i++) {
-            System.out.print(dateFormatSymbols.getMonths()[i] + ", ");
-        }
-        System.out.println(dateFormatSymbols.getMonths()[11]);
+        for (var month : dateFormatSymbols.getMonths())
+            System.out.println(month + ", ");
+
         System.out.println(message.getToday() + " " + DateFormat.getDateInstance(DateFormat.MEDIUM, locale).format(new Date()));
     }
 }
